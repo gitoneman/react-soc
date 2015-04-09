bb25d551afec5e68875c148cb33e95816e1706d5
lujiabz <lujiabz@163.com>
2015-04-09 11:21:47 +0800

    finish accordion commonent

--- js/common/accordion/accordion.react.js
+++ js/common/accordion/accordion.react.js
@@ -1,42 +1,39 @@
 var React = require('react');
+var AccordionItem = require('./accordion-item.react');
 
 var accordion = React.createClass({
+	getInitialState: function() {
+		return {
+			now:null
+		};
+	},
 	componentWillMount: function() {
-
+		
 	},
 	componentWillUnmount: function() {
 
 	},
 	render: function() {
+		var menu = this.props.data;
+		var menus = [];
+		var now = this.state.now;
+
+
+		for(var k in menu){
+			menus.push(<AccordionItem head={k} data={menu[k]} onclick={this._clickHead} active={now == k?true:false}/>)
+			console.log(now == k?true:false)
+		}
+		console.log(menus)
 		return (
 			<div className='accordion'>
-				<div className='accordion-item'>
-					<div className='accordion-item-head'>head1</div>
-					<ul className='accordion-item-content'>
-						<li>item1</li>
-						<li>item2</li>
-						<li>item3</li>
-					</ul>
-				</div>
-				<div className='accordion-item'>
-					<div className='accordion-item-head'>head2</div>
-					<ul className='accordion-item-content'>
-						<li>item1</li>
-						<li>item2</li>
-						<li>item3</li>
-					</ul>
-				</div>
-				<div className='accordion-item'>
-					<div className='accordion-item-head'>head2</div>
-					<ul className='accordion-item-content'>
-						<li>item1</li>
-						<li>item2</li>
-						<li>item3</li>
-					</ul>
-				</div>
-
+				{menus}
 			</div>
 		);
+	},
+	_clickHead:function(o){
+		this.setState({
+			now:o.head 
+		});
 	}
 
 });
--- js/components/report/report.react.js
+++ js/components/report/report.react.js
@@ -1,11 +1,79 @@
 var React = require('react');
-var Submenu = require('../../common/accordion/accordion.react')
+var Submenu = require('../../common/accordion/accordion.react');
+var menu = {
+	"我的菜单":[
+		{
+			name:"侧栏菜单一",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单二",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单三",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单四",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单五",
+			href:"/"
+		}
+	],
+	"我的菜单二":[
+		{
+			name:"侧栏菜单一",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单二",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单三",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单四",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单五",
+			href:"/"
+		}
+	],
+	"我的菜单三":[
+		{
+			name:"侧栏菜单一",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单二",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单三",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单四",
+			href:"/"
+		},
+		{
+			name:"侧栏菜单五",
+			href:"/"
+		}
+	]
+}
 
 var report = React.createClass({
 	render: function() {
 		return (
 			<div className="submenu">
-				<Submenu/>
+				<Submenu data={menu}/>
 			</div>
 			
 		);
